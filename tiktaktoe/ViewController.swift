45ab81fad34a678ceffc473cced0aa0f287610e4
Timo Bell <timo@andre-bell.de>
2022-01-06 20:37:19 +0100

    Initial Commit

---
+++ tiktaktoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tiktaktoe
+//
+//  Created by Timo Bell on 06.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
