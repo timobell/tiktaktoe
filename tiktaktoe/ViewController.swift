ad4e9b8c50c907736ac7569c80864c99fd838ab7
Timo Bell <timo@andre-bell.de>
2022-01-06 21:22:31 +0100

    tiktaktoe

--- tiktaktoe/ViewController.swift
+++ tiktaktoe/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tttlabel: UILabel!
+    @IBOutlet weak var button1label: UIButton!
+    @IBOutlet weak var button2label: UIButton!
+    @IBOutlet weak var button3label: UIButton!
+    @IBOutlet weak var button4label: UIButton!
+    @IBOutlet weak var button5label: UIButton!
+    @IBOutlet weak var button6label: UIButton!
+    @IBOutlet weak var button7label: UIButton!
+    @IBOutlet weak var button8label: UIButton!
+    @IBOutlet weak var button9label: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tttlabel.text = "TikTakToe"
+        tttlabel.textColor = .black
+        tttlabel.textAlignment = .center
+        
     }
-
+    @IBAction func button1(_ sender: UIButton) {
+        button1label.setTitle( "", for: .normal)
+    }
+    
 
 }
 
